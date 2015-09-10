73c748fd57b8f15a9c4b524a8515b8a72328614b
IrinaCodixis <ijumamuradova@codixis.com>
2015-09-10 08:31:18 -0400

    generate CRUD

---
+++ src/Mipa/UserBundle/Form/SubscriptionType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Mipa\UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class SubscriptionType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('type')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Mipa\UserBundle\Entity\Subscription'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'mipa_userbundle_subscription';
+    }
+}
