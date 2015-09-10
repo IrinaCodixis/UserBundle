67fbe6544056e6e2d550678ee6da476bd5c0594b
IrinaCodixis <ijumamuradova@codixis.com>
2015-09-10 09:12:40 -0400

    CRUD for user

---
+++ src/Mipa/UserBundle/Form/UserType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Mipa\UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UserType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('gender')
+            ->add('address')
+            ->add('email')
+            ->add('subscription')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Mipa\UserBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'mipa_userbundle_user';
+    }
+}
