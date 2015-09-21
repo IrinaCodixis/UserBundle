7c835101a3d50eac48c348aa23de1677dd4db95f
IrinaCodixis <ijumamuradova@codixis.com>
2015-09-21 13:32:18 +0300

    fix23 for new function for csv

--- src/Mipa/UserBundle/Controller/UserController.php
+++ src/Mipa/UserBundle/Controller/UserController.php
@@ -271,9 +271,9 @@ class UserController extends Controller
 				//fclose($fp);
             }
         );
-        //$response->headers->set('Content-Type', 'application/force-download');
-        //$response->headers->set('Content-Disposition', 'attachment; filename="export.csv"');
-		return $this->redirect($this->generateUrl('mipa_user'));
+        $response->headers->set('Content-Type', 'application/csv');
+        $response->headers->set('Content-Disposition', 'attachment; filename="export.csv"');
+		return $response;
     }
 	
 }
