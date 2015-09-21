a14c21c94c271f466e4b8bdf84d108cec2d89e24
IrinaCodixis <ijumamuradova@codixis.com>
2015-09-21 12:04:48 +0300

    fix13 for new function for csv

--- src/Mipa/UserBundle/Controller/UserController.php
+++ src/Mipa/UserBundle/Controller/UserController.php
@@ -250,7 +250,7 @@ class UserController extends Controller
         $response->setCallback(
             function () use ($results) {
 				//$file = "php://output";
-				$fp= fopen('php://output', 'r+');
+				$fp= fopen('/var/www/irina-dev.codixis.com/www/', 'w');
                 //$handle = fopen('php://output', 'r+');
                 foreach ($results as $row) {
                     //array list fields you need to export
