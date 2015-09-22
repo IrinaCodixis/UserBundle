022c8283861c760db1b8c9f267331f8afa940c17
IrinaCodixis <ijumamuradova@codixis.com>
2015-09-22 13:27:20 +0300

    edit3 command to include exportCSV function

--- src/Mipa/UserBundle/Command/FtpCommand.php
+++ src/Mipa/UserBundle/Command/FtpCommand.php
@@ -26,7 +26,7 @@ class FtpCommand extends ContainerAwareCommand
         //connexion BD
         $em = $container->get('doctrine')->getManager('default');
        
-       $csv= $em->getRepository('MipaUserBundle:User')->exportCSVAction();
+       $csv= $em->exportCSVAction();
          
 		if(isset($csv)){
 			$output->writeln("Files saved");
