28a098918770184f0143a7352e90edff31d30675
IrinaCodixis <ijumamuradova@codixis.com>
2015-09-23 14:59:45 +0300

    fix20 command to include exportCSV function

--- src/Mipa/UserBundle/Command/FtpCommand.php
+++ src/Mipa/UserBundle/Command/FtpCommand.php
@@ -18,38 +18,6 @@ use Mipa\UserBundle\Form\UserType;
 class FtpCommand extends ContainerAwareCommand
 {
 	
-	public function exportCSVAction()
-    {
-        $container=$this->getApplication()->getKernel()->getContainer();
-      
-        //connexion BD
-        $em = $container->get('doctrine')->getManager('default');
-        $results = $em->getRepository('MipaUserBundle:User')->findAll();
-
-        $response = new StreamedResponse();
-        $response->setCallback(
-            function () use ($results) {
-				$file = '/var/www/irina-dev.codixis.net/www/files/export_'.date("Y_m_d").'.csv';
-				$fp= fopen($file, 'w');
-                //$handle = fopen('php://output', 'r+');
-                foreach ($results as $row) {
-                    //array list fields you need to export
-                    $data = array(
-                        $row->getId(),
-                        $row->getName(),
-						$row->getGender(),
-                        $row->getAddress(),
-						$row->getEmail(),
-                    );
-                    fputcsv($fp, $data);
-                }
-                fclose($fp);
-				
-			}
-        );
-        return $response;
-    }
-	
     protected function configure()
     {
         $this
@@ -68,53 +36,53 @@ class FtpCommand extends ContainerAwareCommand
         $em = $container->get('doctrine')->getManager('default');
         $results = $em->getRepository('MipaUserBundle:User')->findAll();
 
-       $file = '/var/www/irina-dev.codixis.net/www/files/export_'.date("Y_m_d").'.csv';
-				$fp= fopen($file, 'w');
-                //$handle = fopen('php://output', 'r+');
-                foreach ($results as $row) {
-                    //array list fields you need to export
-                    $data = array(
-                        $row->getId(),
-                        $row->getName(),
-						$row->getGender(),
-                        $row->getAddress(),
-						$row->getEmail(),
-                    );
-                    fputcsv($fp, $data);
-                }
-                fclose($fp);
-				
-			}
+		$file = '/var/www/irina-dev.codixis.net/www/files/export_'.date("Y_m_d").'.csv';
+		$fp= fopen($file, 'w');
+               
+        foreach ($results as $row) {
+        //array list fields you need to export
+        $data = array(
+			$row->getId(),
+			$row->getName(),
+			$row->getGender(),
+			$row->getAddress(),
+			$row->getEmail(),
+         );
+		fputcsv($fp, $data);
+        }
+        fclose($fp);
+		$output->writeln("File was saved");		
+	
 
        //envoie ftp
-//        $params = $container->getParameter('user')['ftp'];
-//        if(isset($params)){
-//        	//ftp : $conn_id = ftp_connect($params['server']);
-//        	//sftp
-//        	$conn_id = ssh2_connect($params['server'], $params['port']);
-//        	if($conn_id===false){
-//        		$output->writeln("Connection failed");
-//        		exit;
-//        	}
+        $params = $container->getParameter('user')['ftp'];
+        if(isset($params)){
+        	//ftp : $conn_id = ftp_connect($params['server']);
+        	//sftp
+        	$conn_id = ssh2_connect($params['server'], $params['port']);
+        	if($conn_id===false){
+        		$output->writeln("Connection failed");
+        		exit;
+        	}
         	
         	//ftp authentification $login_result = ftp_login($conn_id, $params['login'],$params['password']);
-//        	$login_result = ssh2_auth_password($conn_id, $params['login'],$params['password']);
+        	$login_result = ssh2_auth_password($conn_id, $params['login'],$params['password']);
         	 
-//        	$remote_file = $params['path'].'export_'.date("Y_m_d").'.csv';
-//        	$output->writeln($file);
- //       	$output->writeln($remote_file);
+        	$remote_file = $params['path'].'export_'.date("Y_m_d").'.csv';
+        	$output->writeln($file);
+			$output->writeln($remote_file);
         	
         	//ftp envoie fichier if (ftp_put($conn_id, $remote_file, $file, FTP_ASCII)) {
         	//
-//        	if (ssh2_scp_send($conn_id,  $file, $remote_file, 0644)) {
-//        		$output->writeln("file transferred");
-//        	} else {
-//        		$output->writeln("Failed transfer");
-//        	}
+        	if (ssh2_scp_send($conn_id,  $file, $remote_file, 0644)) {
+        		$output->writeln("file transferred");
+        	} else {
+        		$output->writeln("Failed transfer");
+        	}
         //	close the connection
         	//ftp_close($conn_id);
-//        }
+        }
       
         
-   // }
+	}
 }
