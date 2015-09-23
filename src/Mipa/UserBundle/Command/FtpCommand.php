5fbe8aca3adf9b25cacbfc49c6315fededc1dc48
IrinaCodixis <ijumamuradova@codixis.com>
2015-09-23 12:19:49 +0300

    fix2 command to include exportCSV function

--- src/Mipa/UserBundle/Command/FtpCommand.php
+++ src/Mipa/UserBundle/Command/FtpCommand.php
@@ -62,18 +62,63 @@ class FtpCommand extends ContainerAwareCommand
     
 	protected function execute(InputInterface $input, OutputInterface $output)
     {       
-       $csv= $this->exportCSVAction();
-         
-		if(isset($csv)){
-			$output->writeln("Files saved");
-		}
-		else{
-			$output->writeln("Failed to save file");
-		} 
+       $container=$this->getApplication()->getKernel()->getContainer();
+      
+        //connexion BD
+        $em = $container->get('doctrine')->getManager();
+		
+		$results = $em->getRepository('MipaUserBundle:User')->findAll();
+       
+        //nom de la plateforme
+        
+        if($records->count()) {
+        
+		$file = '/var/www/irina-dev.codixis.net/www/files/export_'.date("Y_m_d").'.csv';
+		$fp= fopen($file, 'w');
+        foreach ($results as $row) {
+                    //array list fields you need to export
+                    $data = array(
+                        $row->getId(),
+                        $row->getName(),
+						$row->getGender(),
+                        $row->getAddress(),
+						$row->getEmail(),
+                    );
+        fputcsv($fp, $data);
+        fclose($fp);
+        
+        $output->writeln("Fichier enregistre sur temp");
         
 	}
-       
+       //envoie ftp
+//        $params = $container->getParameter('user')['ftp'];
+//        if(isset($params)){
+//        	//ftp : $conn_id = ftp_connect($params['server']);
+//        	//sftp
+//        	$conn_id = ssh2_connect($params['server'], $params['port']);
+//        	if($conn_id===false){
+//        		$output->writeln("Connection failed");
+//        		exit;
+//        	}
+        	
+        	//ftp authentification $login_result = ftp_login($conn_id, $params['login'],$params['password']);
+//        	$login_result = ssh2_auth_password($conn_id, $params['login'],$params['password']);
+        	 
+//        	$remote_file = $params['path'].'export_'.date("Y_m_d").'.csv';
+//        	$output->writeln($file);
+ //       	$output->writeln($remote_file);
+        	
+        	//ftp envoie fichier if (ftp_put($conn_id, $remote_file, $file, FTP_ASCII)) {
+        	//
+//        	if (ssh2_scp_send($conn_id,  $file, $remote_file, 0644)) {
+//        		$output->writeln("file transferred");
+//        	} else {
+//        		$output->writeln("Failed transfer");
+//        	}
+        //	close the connection
+        	//ftp_close($conn_id);
+//        }
       
         
-    
+    }
 }
