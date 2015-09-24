d3aed2a3ab60d73e9e1cb2a52411867a84d26dba
IrinaCodixis <ijumamuradova@codixis.com>
2015-09-24 09:33:38 -0400

    create crud for cron

---
+++ src/Mipa/UserBundle/Form/CronTaskType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Mipa\UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class CronTaskType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('commands')
+            ->add('interval')
+            ->add('lastrun')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Mipa\UserBundle\Entity\CronTask'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'mipa_userbundle_crontask';
+    }
+}
